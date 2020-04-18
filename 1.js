cc73f1df1b9a35404d9793d8bff002f216d41479
eddie0mohammed <me3dun@gmail.com>
2020-04-18 14:43:59 +0400

    new algo

--- 1.js
+++ 1.js
@@ -1,53 +1,15 @@
-function tickets(peopleInLine){
-  // ...
-  let vasya = {};
-  if (peopleInLine[0] !== 25){
-    return 'NO'; 
-  }
-  
-  for (let i = 0; i < peopleInLine.length; i++){
-  
-  let remainder = peopleInLine[i] - 25;
-  console.log(remainder);
-  if (remainder === 75){
-    if (vasya['50'] >= 1 && vasya['25'] >= 1){
-      vasya['50']--;
-      vasya['25']--;
-    }else if (vasya['25'] >= 3){
-      vasya['25'] -= 3;
-    }else{
-      return 'NO';
-    }
-  }else if (remainder === 50){
-    if (vasya['50'] >= 1){
-        vasya['50']--;
-      }else if (vasya['25'] >= 2){
-        vasya['25'] -= 2;
-      }else{
-        return 'NO'
-      }
-  }else if (remainder === 25){
-    if (vasya['25'] >= 1){
-      vasya['25']--;
-    }else{
-      return 'NO';
-    }
-  }else{
-    // return 'NO'
-  }
+
+const solution = (arr) => {
+
+  for (let i = 0; i < (arr.length / 2); i++){
+
+    let temp = arr[i];
+    arr[i] = arr[arr.length - 1 - i];
+    arr[arr.length - 1 - i] = temp
+
     
-  
-  if (vasya.hasOwnProperty(peopleInLine[i])){
-      vasya[peopleInLine[i]]++;
-    }else{
-      vasya[peopleInLine[i]] = 1;
-  }  
-  
   }
-  // console.log(vasya);
-  return 'YES'
+  return arr;
 }
 
-// console.log(tickets([25, 25, 50]))
-console.log(tickets([25, 100]));
-// console.log(tickets([25, 25, 50, 50, 100]));
+console.log(solution([1, 2, 3, 4, 5]));

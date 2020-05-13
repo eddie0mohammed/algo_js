4b866cfe4ee23d434a831db0aebc53c9c1ff6dde
eddie0mohammed <me3dun@gmail.com>
2020-05-13 15:07:27 +0400

    new algo

--- 1.js
+++ 1.js
@@ -1,17 +1,16 @@
 
-const solution = (arr1, arr2) => {
 
-  if (arr1.length !== arr2.length){
-    return ;
+const add1 = () => {
+
+  let a = 1;
+
+  const test = function() {
+    console.log
   }
 
-  for (let i = 0; i < arr1.length; i++){
-
-    let temp = arr2[arr2.length - 1 - i];
-    arr2[arr2.length - 1 - i] = arr1[i];
-    arr1[i] = temp;
-  }
-  return [arr1, arr2];
+  return test
 }
 
-console.log(solution(['a', 'b', 'c'], [1, 2, 3]))
+console.log(add1());
+console.log(add1());
+console.log(add1());

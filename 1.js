9ec0f19ad73813123053dbbf4f56568e2c5bd464
eddie0mohammed <me3dun@gmail.com>
2020-05-15 14:11:16 +0400

    new algo

--- 1.js
+++ 1.js
@@ -1,25 +1,34 @@
-
-// const add = (() => {
-  
-//   let a = 1;
-//   return function(){
-//     a++;
-//     return a;
-//   }
-// })();
-
-
-const add = () => {
-
-  let a = 1;
-  return function(){
-    a++;
-    return a;
+function flatten(obj) {
+    // Implement function here
+    console.log("You can output things here for testing");
+    
+    let newArr = [];
+    
+    let arr = Object.keys(obj);
+    if (arr.length === 0){
+      return [];
+    }
+    
+    
+    
+    
+    newArr.push({id: obj.id});
+    
+    
+    
+    if (obj.children){
+      obj.children.forEach(elem => newArr.push(elem));
+    }
+    
+    return newArr;
   }
-}
-
-
-console.log(add());
-console.log(add());
-console.log(add());
-console.log(add());
+  
+  var exampleInput = {
+    id:"abc",
+    children: [
+        {id: 'x'},
+        {id: 'y'}
+    ]
+  };
+  
+  console.log(flatten(exampleInput))

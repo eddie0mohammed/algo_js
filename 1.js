8cfa3e439b36fc12ab5d36effabe74ab2c8440ae
eddie0mohammed <me3dun@gmail.com>
2020-06-29 09:08:13 +0400

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,17 +1,1 @@
-
-
-const mainState = {
-    "1": ["1"],
-    "2": ["2"],
-    "3": ["3"]
-}
-
-
-const objToSend = {
-    "a": "test1",
-    "b": "test2",
-    "c": "test3",
-    "test": mainState
-}
-
-console.log(JSON.stringify(objToSend));
+//
